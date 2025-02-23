bb9d4f2d2d614f533fb2704c5e62284f0a8a1f8b
Proskurenko Danylo <proskurenko.danylo@gmail.com>
2025-02-23 11:57:07 +0000

    trees outline views done
    Trees outline views done.

---
+++ Trees/Trees/BinaryNode.cs
@@ -0,0 +1,59 @@
+﻿BinaryNode<string> binaryTree = new BinaryNode<string>("Root");
+binaryTree.AddLeft("A");
+binaryTree?.LeftNode?.AddLeft("C");
+binaryTree?.LeftNode?.AddRight("D");
+
+binaryTree?.AddRight("B");
+binaryTree?.RightNode?.AddRight("E");
+binaryTree?.RightNode?.RightNode?.AddLeft("F");
+
+Console.WriteLine(binaryTree);
+Console.WriteLine(binaryTree?.LeftNode);
+
+public class BinaryNode<T>(T value)
+    {
+        public T Value { get; } = value;
+        public BinaryNode<T>? LeftNode { get; set; }
+        public BinaryNode<T>? RightNode { get; set; }
+
+        public void AddLeft(T value)
+        {
+            LeftNode = new BinaryNode<T>(value);
+        }
+
+        public void AddRight(T value)
+        {
+            RightNode = new BinaryNode<T>(value);
+        }
+
+        public override string ToString()
+        {
+            return ToString("");
+        }
+
+        public string ToString(string spaces)
+        {
+            var result = spaces + Value?.ToString() + ':';
+            if (LeftNode != null)
+            {
+                result += "\n";
+                result += LeftNode.ToString(spaces + "  ");
+            } 
+            else if (RightNode != null)
+            {
+                result += "\n" + spaces + "  None";
+            }
+
+            if (RightNode != null)
+            {
+                result += "\n";
+                result += RightNode.ToString(spaces + "  ");
+            }
+            else if (LeftNode != null)
+            {
+                result += "\n" + spaces + "  None";
+            }
+
+            return result;
+        }
+    }
---
+++ Trees/Trees/NaryNode.cs
@@ -0,0 +1,42 @@
+﻿
+NaryNode<string> NaryTree = new NaryNode<string>("Root");
+NaryTree.AddChild("A");
+NaryTree.AddChild("B");
+NaryTree.AddChild("C");
+NaryTree.Children[0].AddChild("D");
+NaryTree.Children[0].AddChild("E");
+NaryTree.Children[0].Children[0].AddChild("G");
+NaryTree.Children[2].AddChild("F");
+NaryTree.Children[2].Children[0].AddChild("H");
+NaryTree.Children[2].Children[0].AddChild("I");
+
+Console.WriteLine(NaryTree.ToString());
+
+public class NaryNode<T>(T value)
+{
+    public T Value { get; } = value;
+    public List<NaryNode<T>> Children { get; } = new();
+
+    public void AddChild(T value)
+    {
+        Children.Add(new NaryNode<T>(value));
+    }
+
+    public override string ToString()
+    {
+        return ToString("");
+    }
+
+    public string ToString(string spaces)
+    {
+        var result = spaces + Value?.ToString() + ':';
+        if (Children.Count != 0)
+        {
+            foreach (var child in Children)
+            {
+                result += "\n" + child.ToString(spaces + "  ");
+            }
+        }
+        return result;
+    }
+}
